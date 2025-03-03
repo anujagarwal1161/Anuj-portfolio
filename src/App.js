0028757e2340f40edf38551b8b81674cfa86395c
Anuj Agarwal <anuj.agarwal@pelocal.com>
2025-03-04 03:44:24 +0530

    removed footer

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import Banner from "./components/banner/Banner";
 import Contact from "./components/contact/Contact";
 import Features from "./components/features/Features";
-import Footer from "./components/footer/Footer";
 import FooterBottom from "./components/footer/FooterBottom";
 import Navbar from "./components/navbar/Navbar";
 import Projects from "./components/projects/Projects";
@@ -20,7 +19,6 @@ function App() {
         <Resume />
         {/* <Testimonial /> */}
         <Contact />
-        <Footer />
         <FooterBottom />
       </div>
     </div>
